05253b297f3bddb4603fd4a8b906db08100008af
gs0428 <lider28@naver.com>
2024-05-30 11:51:51 +0900

    feat: 화면에 지도 띄우기 구현

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import {NaverMapView} from '@mj-studio/react-native-naver-map';
+import React, {useEffect} from 'react';
+
+import {Platform, Text, View} from 'react-native';
+import {
+  request,
+  PERMISSIONS,
+  requestLocationAccuracy,
+  requestMultiple,
+} from 'react-native-permissions';
+
+function App(): React.JSX.Element {
+  // useEffect는 단순히 컴포넌트가 mount될 때 호출해주기 위해서 사용되었습니다.
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      request(PERMISSIONS.IOS.LOCATION_ALWAYS).then(status => {
+        console.log(`Location request status: ${status}`);
+        if (status === 'granted') {
+          requestLocationAccuracy({
+            purposeKey: 'common-purpose', // replace your purposeKey of Info.plist
+          })
+            .then(accuracy => {
+              console.log(`Location accuracy is: ${accuracy}`);
+            })
+            .catch(e => {
+              console.error(`Location accuracy request has been failed: ${e}`);
+            });
+        }
+      });
+    }
+    if (Platform.OS === 'android') {
+      requestMultiple([
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+      ])
+        .then(status => {
+          console.log(`Location request status: ${status}`);
+        })
+        .catch(e => {
+          console.error(`Location request has been failed: ${e}`);
+        });
+    }
+  }, []);
+  return <NaverMapView style={{flex: 1}} />;
+}
+
+export default App;
