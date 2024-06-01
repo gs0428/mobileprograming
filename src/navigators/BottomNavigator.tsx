540b6a7343e1bb648c9396bddd592b722657dbb1
gs0428 <lider28@naver.com>
2024-06-01 14:36:15 +0900

    feat: home 화면 구현

--- src/navigators/BottomNavigator.tsx
+++ src/navigators/BottomNavigator.tsx
@@ -2,15 +2,49 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Festa from '../screens/Festa';
 import Place from '../screens/Place';
+import SvgIcon from '../assets/SvgIcon';
+import {colors} from '../colors';
 
 const BottomTab = createBottomTabNavigator();
 
 export default function BottomNavigator() {
   return (
-    <BottomTab.Navigator initialRouteName="Home">
-      <BottomTab.Screen name="Festa" component={Festa} />
-      <BottomTab.Screen name="Home" component={Home} />
-      <BottomTab.Screen name="Place" component={Place} />
+    <BottomTab.Navigator
+      initialRouteName="홈"
+      screenOptions={{tabBarStyle: {borderTopColor: colors.grey100}}}>
+      <BottomTab.Screen
+        name="축제"
+        component={Festa}
+        options={{
+          tabBarIcon: ({focused}) =>
+            focused ? <SvgIcon.FestaFilled /> : <SvgIcon.Festa />,
+          tabBarActiveTintColor: colors.black,
+          tabBarInactiveTintColor: colors.grey200,
+          headerShown: false,
+        }}
+      />
+      <BottomTab.Screen
+        name="홈"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) =>
+            focused ? <SvgIcon.HomeFilled /> : <SvgIcon.Home />,
+          tabBarActiveTintColor: colors.black,
+          tabBarInactiveTintColor: colors.grey200,
+          headerShown: false,
+        }}
+      />
+      <BottomTab.Screen
+        name="명소"
+        component={Place}
+        options={{
+          tabBarIcon: ({focused}) =>
+            focused ? <SvgIcon.PlaceFilled /> : <SvgIcon.Place />,
+          tabBarActiveTintColor: colors.black,
+          tabBarInactiveTintColor: colors.grey200,
+          headerShown: false,
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
