49b3a65cc7c03eea29c697e51b24f7af13cd3ba6
gs0428 <lider28@naver.com>
2024-06-02 02:35:41 +0900

    feat: icon 추가

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,6 +1,7 @@
 export const colors = {
   black: '#333333',
-  grey100: '#B1B1B1',
-  grey200: '#919191',
+  grey100: '#C9C9C9',
+  grey200: '#B1B1B1',
+  grey300: '#919191',
   white: '#ffffff',
 };
